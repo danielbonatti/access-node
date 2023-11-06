47225e02a754f9756979f244c8710d88084f91c3
Daniel Bonatti <bonattidaniel@hotmail.com>
2023-11-06 14:34:52 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,9 +1,7 @@
 <?php
   $xmlContent = $argv[1];     // Arquivo xml em string
-  $nodoRaiz = $argv[2];       // Nó raiz para acesso aos dados de retorno
-
+  
   $xmlObject = simplexml_load_string($xmlContent);
-  $result = $xmlObject->xpath('//'.$nodoRaiz);
-  $jsonString = json_encode($result, JSON_PRETTY_PRINT);
+  $jsonString = json_encode($xmlObject, JSON_PRETTY_PRINT);
   echo $jsonString;
 ?>
